3a547302d70fc9875ed2d4d1ce9a5ac8c241f6f5
burnigman14 <burnigman14@seznam.cz>
2017-05-30 13:51:34 +0200

    first commit

---
+++ test.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Copyright (C) 2016 Tomáš Horak <burnigman14@seznam.cz>.
+ */
+
+namespace Burnigman\test;
+
+
+
+class Test {
+
+  
+    public function __construct() {
+
+     
+    }
+
+    public function sayHello() {
+            
+           echo "Hello";
+          
+    }       
+       
+
+  
+}
